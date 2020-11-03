3c7e264a391438144e48589af68284ed1ce4d22a
Mintsjeuh <jan.minne.holwerda@student.nhlstenden.com>
2020-11-03 14:51:20 +0100

    Sensor_values processing

--- hardware/filecontrol.py
+++ hardware/filecontrol.py
@@ -1,8 +1,9 @@
 import os
 import time
+import mysql.connector
 
 xdays = 1
-path = '/home/pi/Desktop/project/videos'
+path = '/home/pi/Desktop/project/hardware/videos'
 now = time.time()
 
 files = os.listdir(path)
@@ -13,6 +14,22 @@ print("===========================" + "=" * len(str(xdays)) + "====")
 for f in files:
 	if os.stat("videos/" + f).st_mtime < now - (xdays * 86400):
 		print(f)
+		database = mysql.connector.connect(
+			host="imac-van-pieter",
+			user="root",
+			password="root",
+			database="camaro",
+			port="3306"
+		)
+		mycursor = database.cursor()
+
+		sql = "DELETE FROM `activities` (`ID`, `timestamp`, `valid`, `userid`) VALUES (NULL, %s, %s, %s)"
+		val = (f, '1', '1')
+		mycursor.execute(sql, val)
+
+		database.commit()
+
+		print(mycursor.rowcount, "record removed.")		
 		os.system("sudo rm videos/" + f)
 
 print("\nList all files newer than " + str(xdays) + " days")
