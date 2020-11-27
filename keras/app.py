b1f354a6f4b3d5f6a6b5a28b53a7dad0e988438c
Pieter Boersma <pieterboersma20@gmail.com>
2020-11-27 13:53:06 +0100

    Beter model

--- keras/app.py
+++ keras/app.py
@@ -1,14 +1,16 @@
-from flask import Flask
-from flask_cors import CORS
+#from flask import Flask
+#from flask_cors import CORS
 
 from CustomModel import CustomModel
 
-app = Flask(__name__)
-CORS(app)
+#app = Flask(__name__)
+#CORS(app)
 
 
-@app.route("/")
-def predict():
-    model = CustomModel()
-    model.define_model()
-    return model.predict('data/validation/cats/cat.4030.jpg')
+# @app.route("/")
+# def predict():
+
+model = CustomModel()
+model.define_model()
+model.train()
+#print(model.predict('data/validation/cats/cat.4030.jpg'))
